ce959c8d6af0a912455c7de9f865eacd6d03ea35
Matheus Marques <matheuspadilha.dev@gmail.com>
2026-06-01 21:27:53 +0100

    ConversationService WIP

--- src/main/java/com/padda/helpmepet/service/ChatService.java
+++ src/main/java/com/padda/helpmepet/service/ChatService.java
@@ -1,4 +1,22 @@
 package com.padda.helpmepet.service;
 
+import com.padda.helpmepet.dto.ChatRequest;
+import com.padda.helpmepet.dto.ChatResponse;
+import com.padda.helpmepet.model.Conversation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class ChatService {
+
+    private final OllamaService ollamaService;
+    private final ConversationService conversationService;
+    private final String systemPrompt;
+
+    public ChatResponse chat(ChatRequest request, Long userId){
+
+        Conversation conv = conversationService.getOrCreate(request.conversationId(), userId);
+
+    }
 }
